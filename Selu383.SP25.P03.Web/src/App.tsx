d3025ff18e9776e4f113a59c39bc6a82c2cf1e2e
matthewvidacovich <194949094+matthewvidacovich@users.noreply.github.com>
2025-03-19 17:34:23 -0500

    wip: login works

--- Selu383.SP25.P03.Web/src/App.tsx
+++ Selu383.SP25.P03.Web/src/App.tsx
@@ -5,6 +5,8 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
@@ -24,8 +26,47 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
+      <form action="/api/theaters" method="post" className="form-example" onSubmit={(e) => saveData(e)}>
+        <div className="form-example">
+          <label htmlFor="name">Enter your username: </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-example">
+          <label htmlFor="password">Enter your password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-example">
+          <input type="submit" value="Login" />
+        </div>
+      </form>
     </>
   );
+
+  function saveData(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    fetch("/api/authentication/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(e);
+  }
 }
 
 export default App;
